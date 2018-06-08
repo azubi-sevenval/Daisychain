5d9ef437609a188c858bce53470224c10436f0bb
jonas.kuepper <jonas.kuepper@sevenval.com>
2018-06-08 12:11:47 +0200

    Added gitignore

--- FlowerFieldApp/Daisychain.cs
+++ FlowerFieldApp/Daisychain.cs
@@ -1,32 +1,21 @@
-/*
- * fn@gso-koeln.de 2018
- */
 using System;
 using System.Collections;
 
-namespace FlowerFieldApp
-{
-    public class Daisychain<T> : IEnumerable, IEnumerator
-    {
+namespace FlowerFieldApp {
+    public class Daisychain<T> : IEnumerable, IEnumerator {
         private Daisy<T> first = null;
         private Daisy<T> current = null;
         public int count;
 
-        public void Insert(T value)
-        {
+        public void Insert(T value) {
             Daisy<T> daisy = new Daisy<T>(value);
-            if (first == null)
-            {
+            if (first == null) {
                 first = daisy;
                 current = first;
-            }
-            else
-            {
-                if(current.Next == null)
-                {
+            } else {
+                if(current.Next == null) {
                     current.Next = daisy;
                     current = daisy;
-                    Console.WriteLine(current.Value);
                 }
             }
             count++;
@@ -34,8 +23,7 @@ namespace FlowerFieldApp
 
         public int Count() { return this.count; }
 
-        public void Remove()
-        {
+        public void Remove() {
             current = null;
             count--;
         }
@@ -49,28 +37,22 @@ namespace FlowerFieldApp
             current = first;
         }
 
-        public bool MoveNext()
-        {
-            if (current.Next != null)
-            {
+        public bool MoveNext() {
+            if (current.Next != null) {
                 current = current.Next;
                 return true;
-            }
-            else
-            {
+            } else {
                 return false;
             }
         }
 
         public IEnumerator GetEnumerator() { Reset(); return this; }
 
-        private class Daisy<U>
-        {
+        private class Daisy<U> {
             public readonly U Value;
             public Daisy<U> Next;
 
-            public Daisy(U value, Daisy<U> next = null)
-            {
+            public Daisy(U value, Daisy<U> next = null) {
                 this.Value = value;
                 this.Next = next;
             }
